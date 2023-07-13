c49ab3a7c44525582153cfd13b10a142deaf0b78
SecretBase <5130999+SecretBase@users.noreply.github.com>
2023-07-14 02:33:14 +0800

    feat: line and stations data

---
+++ src/lib/lines.ts
@@ -0,0 +1,19 @@
+export const AEL = 'AEL' as const
+export const EAL = 'EAL' as const
+export const ISL = 'ISL' as const
+export const SIL = 'SIL' as const
+export const TCL = 'TCL' as const
+export const TKL = 'TKL' as const
+export const TML = 'TML' as const
+export const TWL = 'TWL' as const
+
+export const linesName = {
+	[AEL]: 'Airport Express',
+	[EAL]: 'East Rail Line',
+	[ISL]: 'Island Line',
+	[SIL]: 'South Island Line',
+	[TCL]: 'Tung Chung Line',
+	[TKL]: 'Tseung Kwan O Line',
+	[TML]: 'Tuen Ma Line',
+	[TWL]: 'Tsuen Wan Line',
+} as const
---
+++ src/lib/stations.ts
@@ -0,0 +1,179 @@
+export const ADM = 'ADM' as const
+export const AIR = 'AIR' as const
+export const AUS = 'AUS' as const
+export const AWE = 'AWE' as const
+export const CAB = 'CAB' as const
+export const CEN = 'CEN' as const
+export const CHW = 'CHW' as const
+export const CIO = 'CIO' as const
+export const CKT = 'CKT' as const
+export const CSW = 'CSW' as const
+export const DIH = 'DIH' as const
+export const ETS = 'ETS' as const
+export const EXC = 'EXC' as const
+export const FAN = 'FAN' as const
+export const FOH = 'FOH' as const
+export const FOT = 'FOT' as const
+export const HAH = 'HAH' as const
+export const HEO = 'HEO' as const
+export const HFC = 'HFC' as const
+export const HIK = 'HIK' as const
+export const HKU = 'HKU' as const
+export const HOK = 'HOK' as const
+export const HOM = 'HOM' as const
+export const HUH = 'HUH' as const
+export const JOR = 'JOR' as const
+export const KAT = 'KAT' as const
+export const KET = 'KET' as const
+export const KOT = 'KOT' as const
+export const KOW = 'KOW' as const
+export const KSR = 'KSR' as const
+export const KWF = 'KWF' as const
+export const KWH = 'KWH' as const
+export const LAK = 'LAK' as const
+export const LCK = 'LCK' as const
+export const LET = 'LET' as const
+export const LHP = 'LHP' as const
+export const LMC = 'LMC' as const
+export const LOP = 'LOP' as const
+export const LOW = 'LOW' as const
+export const MEF = 'MEF' as const
+export const MKK = 'MKK' as const
+export const MOK = 'MOK' as const
+export const MOS = 'MOS' as const
+export const NAC = 'NAC' as const
+export const NOP = 'NOP' as const
+export const OCP = 'OCP' as const
+export const OLY = 'OLY' as const
+export const POA = 'POA' as const
+export const PRE = 'PRE' as const
+export const QUB = 'QUB' as const
+export const RAC = 'RAC' as const
+export const SHM = 'SHM' as const
+export const SHS = 'SHS' as const
+export const SHT = 'SHT' as const
+export const SHW = 'SHW' as const
+export const SIH = 'SIH' as const
+export const SKW = 'SKW' as const
+export const SOH = 'SOH' as const
+export const SSP = 'SSP' as const
+export const STW = 'STW' as const
+export const SUN = 'SUN' as const
+export const SUW = 'SUW' as const
+export const SWH = 'SWH' as const
+export const SYP = 'SYP' as const
+export const TAK = 'TAK' as const
+export const TAP = 'TAP' as const
+export const TAW = 'TAW' as const
+export const TIH = 'TIH' as const
+export const TIK = 'TIK' as const
+export const TIS = 'TIS' as const
+export const TKO = 'TKO' as const
+export const TKW = 'TKW' as const
+export const TSH = 'TSH' as const
+export const TST = 'TST' as const
+export const TSW = 'TSW' as const
+export const TSY = 'TSY' as const
+export const TUC = 'TUC' as const
+export const TUM = 'TUM' as const
+export const TWH = 'TWH' as const
+export const TWO = 'TWO' as const
+export const TWW = 'TWW' as const
+export const UNI = 'UNI' as const
+export const WAC = 'WAC' as const
+export const WCH = 'WCH' as const
+export const WKS = 'WKS' as const
+export const YAT = 'YAT' as const
+export const YMT = 'YMT' as const
+export const YUL = 'YUL' as const
+
+export const stationsName = {
+	[ADM]: 'Admiralty',
+	[AIR]: 'Airport',
+	[AUS]: 'Austin',
+	[AWE]: 'AsiaWorld Expo',
+	[CAB]: 'Causeway Bay',
+	[CEN]: 'Central',
+	[CHW]: 'Chai Wan',
+	[CIO]: 'City One',
+	[CKT]: 'Che Kung Temple',
+	[CSW]: 'Cheung Sha Wan',
+	[DIH]: 'Diamond Hill',
+	[ETS]: 'East Tsim Sha Tsui',
+	[EXC]: 'Exhibition Centre',
+	[FAN]: 'Fanling',
+	[FOH]: 'Fortress Hill',
+	[FOT]: 'Fo Tan',
+	[HAH]: 'Hang Hau',
+	[HEO]: 'Heng On',
+	[HFC]: 'Heng Fa Chuen',
+	[HIK]: 'Hin Keng',
+	[HKU]: 'HKU',
+	[HOK]: 'Hong Kong',
+	[HOM]: 'Ho Man Tin',
+	[HUH]: 'Hung Hom',
+	[JOR]: 'Jordan',
+	[KAT]: 'Kai Tak',
+	[KET]: 'Kennedy Town',
+	[KOT]: 'Kowloon Tong',
+	[KOW]: 'Kowloon',
+	[KSR]: 'Kam Sheung Road',
+	[KWF]: 'Kwai Fong',
+	[KWH]: 'Kwai Hing',
+	[LAK]: 'Lai King',
+	[LCK]: 'Lai Chi Kok',
+	[LET]: 'Lei Tung',
+	[LHP]: 'LOHAS Park',
+	[LMC]: 'Lok Ma Chau',
+	[LOP]: 'Long Ping',
+	[LOW]: 'Lo Wu',
+	[MEF]: 'Mei Foo',
+	[MKK]: 'Mong Kok East',
+	[MOK]: 'Mong Kok',
+	[MOS]: 'Ma On Shan',
+	[NAC]: 'Nam Cheong',
+	[NOP]: 'North Point',
+	[OCP]: 'Ocean Park',
+	[OLY]: 'Olympic',
+	[POA]: 'Po Lam',
+	[PRE]: 'Price Edward',
+	[QUB]: 'Quarry Bay',
+	[RAC]: 'Racecourse',
+	[SHM]: 'Shek Mun',
+	[SHS]: 'Sheung Shui',
+	[SHT]: 'Sha Tin',
+	[SHW]: 'Sheung Wan',
+	[SIH]: 'Siu Hong',
+	[SKW]: 'Shau Kei Wan',
+	[SOH]: 'South Horizons',
+	[SSP]: 'Sham Shui Po',
+	[STW]: 'Sha Tin Wai',
+	[SUN]: 'Sunny Bay',
+	[SUW]: 'Sung Wong Toi',
+	[SWH]: 'Sai Wan Ho',
+	[SYP]: 'Sai Ying Pun',
+	[TAK]: 'Tai Koo',
+	[TAP]: 'Tai Po Market',
+	[TAW]: 'Tai Wai',
+	[TIH]: 'Tin Hau',
+	[TIK]: 'Tiu Keng Leng',
+	[TIS]: 'Tin Shui Wai',
+	[TKO]: 'Tseung Kwan O',
+	[TKW]: 'To Kwa Wan',
+	[TSH]: 'Tai Shui Hang',
+	[TST]: 'Tsim Sha Tsui',
+	[TSW]: 'Tsuen Wan',
+	[TSY]: 'Tsing Yi',
+	[TUC]: 'Tung Chung',
+	[TUM]: 'Tuen Mun',
+	[TWH]: 'Tai Wo Hau',
+	[TWO]: 'Tai Wo',
+	[TWW]: 'Tsuen Wan West',
+	[UNI]: 'University',
+	[WAC]: 'Wan Chai',
+	[WCH]: 'Wong Chuk Hang',
+	[WKS]: 'Wu Kai Sha',
+	[YAT]: 'Yau Tong',
+	[YMT]: 'Yau Ma Tei',
+	[YUL]: 'Yuen Long',
+} as const
